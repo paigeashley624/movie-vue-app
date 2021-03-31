bbab4ac42b7a4659633195c43d720fc2251762e4
Paige Morton <paigeashley624@gmail.com>
2021-03-31 09:58:32 -0400

    adds signup login and logout

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,11 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Signup from "../views/Signup.vue";
+import Login from "../views/Login.vue";
+import Logout from "../views/Logout.vue";
+
+
 
 Vue.use(VueRouter);
 
@@ -10,6 +15,22 @@ const routes = [
     name: "Home",
     component: Home,
   },
+  { 
+    path: "/signup", 
+    name: "signup", 
+    component: Signup 
+  },
+  { 
+    path: "/login", 
+    name: "login", 
+    component: Login 
+  },
+  { 
+    path: "/logout", 
+    name: "logout", 
+    component: Logout 
+  },
+
   {
     path: "/about",
     name: "About",
